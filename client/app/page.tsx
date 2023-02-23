ca53c42f7f14fe4b4f6e44733dbb6d650e7d1119
erratinsilentio <kacper.kondracki@wp.pl>
2023-02-23 01:29:01 +0100

    added mesh background

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,5 +3,9 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main>hello world</main>;
+  return (
+    <main className="flex items-center justify-center">
+      <p className="text-3xl">erratinsilentio</p>
+    </main>
+  );
 }
